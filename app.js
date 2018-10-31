200b562337b5997691710be4bb490bdec2f1a9da
anzhari.p@gmail.com <anzhari.p@gmail.com>
2018-10-31 08:48:23 +0700

    Fix npm startup script so you can start the app with 'npm start'

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => res.send(req.headers))
 app.get('/headersinfo', (req, res) => res.send(req.headers))
 
